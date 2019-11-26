b99ccaab11afc3e37fa7396e4a16e0bcf9c6abe2
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-26 04:29:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,8 +40,8 @@ end
 def total_gross(source)
     grand_total = 0 
   column_index = 0
-  while column_index < list_of_directors.size do 
-    grand_total += directors_totals(source)
+  while column_index < source.size do 
+    grand_total += directors_totals(source)[column_index]
     column_index += 1 
   end
   return grand_total
