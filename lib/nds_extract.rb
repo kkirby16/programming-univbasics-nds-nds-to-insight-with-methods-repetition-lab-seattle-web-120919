50cb02a9902ce7bd4b01fde681fa22e624b98fd9
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-26 04:25:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,7 +40,7 @@ end
 def total_gross(source)
     grand_total = 0 
   column_index = 0
-  while column_index < source[:movies].length do 
+  while column_index < source.length do 
     grand_total += directors_totals(source)
     column_index += 1 
   end
